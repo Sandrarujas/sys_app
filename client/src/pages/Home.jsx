9db4a281fb9041932ddafe1d3874d0d5f48f660c
Sandrarujas <srujasarroyo@gmail.com>
2025-06-08 23:32:38 +0200

    Corrigiendo errores

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
     const fetchPosts = async () => {
       try {
         setLoading(true)
-        const res = await axiosInstance.get(`${BASE_URL}/api/posts?page=${page}&limit=5`)
+        const res = await axiosInstance.get(`/api/posts?page=${page}&limit=5`)
         if (res.data?.posts && Array.isArray(res.data.posts)) {
           setPosts(res.data.posts)
           setPagination(res.data.pagination || pagination)
