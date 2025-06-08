58ffe7d1658c9da63afd0230c6217d6d52c881f5
Sandrarujas <srujasarroyo@gmail.com>
2025-06-08 23:36:01 +0200

    Corrigiendo errores

--- client/src/api/axiosInstances.js
+++ client/src/api/axiosInstances.js
@@ -6,18 +6,5 @@ const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
-// Agregar interceptor para enviar el token en cada petición si existe
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token"); // o donde guardes el token
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default axiosInstance;
