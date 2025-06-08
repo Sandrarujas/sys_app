360bd6161c86cf2b2b10b51f25684cd855aa6e20
Sandrarujas <srujasarroyo@gmail.com>
2025-06-08 22:00:20 +0200

    Corrigiendo errores

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -8,7 +8,7 @@ import CommentList from "./CommentList"
 import EditPostModal from "./EditPostModal"
 import styles from "../styles/Post.module.css"
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"
+const BASE_URL = process.env.REACT_APP_API_URL 
 
 const Post = ({ post, onPostUpdate, onPostDelete, onCommentAdded, onLikeToggled }) => {
   const {
