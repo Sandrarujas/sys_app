8052b8b261473af1c0243e4365db45099abd19dc
Sandrarujas <srujasarroyo@gmail.com>
2025-06-08 22:28:27 +0200

    Corrigiendo errores

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -4,6 +4,8 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
+    console.log("ProtectedRoute - loading:", loading, "user:", user);
+
   if (loading) return <div className="loading">Cargando...</div>;
 
   if (!user) return <Navigate to="/login" replace />;
