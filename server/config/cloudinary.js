5a9b075b5c242ad44e1c3d227006124345f6a84a
Sandrarujas <srujasarroyo@gmail.com>
2025-06-09 18:39:37 +0200

    Corrigiendo errores

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,4 +1,23 @@
-const { v2: cloudinary } = require('cloudinary');
-cloudinary.config();
+// config/cloudinary.js
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-module.exports = cloudinary;
+// Configurar con variables de entorno
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Almacenamiento con multer y Cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "uploads", // Carpeta en tu cuenta Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png", "gif"],
+  },
+});
+
+const upload = require("multer")({ storage });
+
+module.exports = upload;
